1d52900a6cb4f6d9d704f972e319183efa8967be
Artem Shapiro <artemshapa1772@gmail.com>
2017-02-11 22:12:59 +0200

    Feature auth. Utility api-client

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,28 @@ export const addCalendarEevent = (event) => {
     event
   }
 }
+
+export const setHeaders = (headers) => (
+  {
+    type: 'SET_HEADERS',
+    header: {
+      access_token: headers['access-token'],
+      client: headers.client,
+      uid: headers.uid
+    }
+  }
+)
+
+export const setFormError = (formName, message) => (
+  {
+    type: 'SET_FORM_ERROR',
+    formName,
+    message
+  }
+)
+
+export const clearFormError = () => (
+  {
+    type: 'CLEAR_FORM_ERROR'
+  }
+)
